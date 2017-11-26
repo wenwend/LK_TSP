4d78fd677497362652dbb37289f36dbbe3dc503a
wenwend <anita.dww@gmail.com>
2017-11-26 01:25:00 -0800

    Create lkMain.cpp

---
+++ lkMain.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#include <fstream>
+#include<string>
+#include<sstream>
+
+//#include "lk.hpp"
+
+using namespace std;
+
+std::vector<int> id;
+std::vector<pair<double, double> > coord;
+
+int main() {
+    int i=0;
+    std::ifstream ifs;
+    ifs.open("tsp_example_3.txt");
+
+    std::string line;
+    int array[500];
+    int n;
+	double x, y;
+
+    while(!ifs.eof()){
+        std::getline(ifs,line);
+
+        std::istringstream input(line);
+        std::string number;
+	i++;
+        while(std::getline(input,number,' ')){
+            std::istringstream iss(number);
+            iss>>n;
+            id.push_back(n);
+            iss>>x>>y;
+            coord.push_back(make_pair(x, y));
+            
+	    //cout<<"done reading";
+        }
+
+    }
+	cout<<"Total is "<<i-1<<" lines.";
+
+	return 0;
+}
